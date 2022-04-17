09f012130da593c11875bd0c59bb303edd027b34
Sinan Şenkul <101463814+SinanSenkul@users.noreply.github.com>
2022-04-18 01:08:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function createNumber(){
+    num1=document.getElementById('tB1').value;
+    num2=document.getElementById('tB2').value;
+    num3=document.getElementById('tB3').value;
+    if(num1==="" || num2==="" || num3===""){
+        alert("Fill in the required fields");
+        return;
+    }
+    num1=document.getElementById('tB1').value*1;
+    num2=document.getElementById('tB2').value*1;
+    num3=document.getElementById('tB3').value*1;
+    if(num1>=num2){
+        alert("First number must be smaller");
+        document.getElementById('tB1').value="";
+        return;
+    }
+    if(num3>(num2-num1+1)){
+        alert(`Maximum of ${num2-num1} numbers can be created`);
+        document.getElementById('tB3').value="";
+        return;
+    }
+    numArrayLength=num2-num1;
+    numArray=new Array();
+    numArray.length=numArrayLength;
+    resArray=new Array();
+    resArray.length=num3;
+    for(i=0; i<=numArrayLength; i++){
+        numArray[i]=num1+i;
+    }
+    for(j=0; j<num3; j++){
+        ranNum=Math.random()*(numArrayLength);
+        ranNum=Math.round(ranNum);
+        while(numArray[ranNum]===null){
+            ranNum=Math.random()*(numArrayLength);
+            ranNum=Math.round(ranNum);
+        }
+        resArray[j]=numArray[ranNum];
+        numArray[ranNum]=null;
+    }
+    document.getElementById("tB4").value=resArray;
+}
